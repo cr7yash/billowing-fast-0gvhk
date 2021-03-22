c11e6d1d24913180963801b3e9398f27f66c8a4b
cr7yash <yashaqua@gmail.com>
2021-03-23 00:19:40 +0530

    Output the ingredients dynamically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,12 +6,19 @@ import classes from "./Burger.css";
 const Burger = (props) => {
   //Convert object to array so that I can map over the values.
   //Map over the keys to obtain the length of the array which is the initial number of ingredients.(See the state)
-  const transformedIngredients = Object.keys(props.ingredients).map((igKey) => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredients key={igKey + i} type={igKey} />;
-    });
-  });
-
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])] //Returns the length of the array
+        .map((_, i) => {
+          return <BurgerIngredients key={igKey + i} type={igKey} />;
+        });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
   return (
     <div className={classes.Burger}>
       <BurgerIngredients type="bread-top" />
